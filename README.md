5506d1dfd11d32d8681554276cedb9134e0bab3c
qo <72982727+qo@users.noreply.github.com>
2022-08-13 20:38:32 +0300

    - Add README.md - Add Lesson 1 Project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Covered Topics
+
+Lesson 1: Selecting Elements in the DOM
+
---
+++ lesson-1/app.js
@@ -0,0 +1,24 @@
+// getElementById
+
+const container = document.getElementById("fav-movies-container");
+console.log(container);
+
+// getElementsByClassName
+
+const title = document.getElementsByClassName("fav-movies-list-item");
+console.log(title);
+
+// getElementsByTagName
+
+const list = document.getElementsByTagName("ul");
+console.log(list);
+
+// querySelector
+
+const listItem = document.querySelector(".fav-movies-list-item");
+console.log(listItem);
+
+// querySelectorAll
+
+const listItems = document.querySelectorAll(".fav-movies-list-item");
+console.log(listItems);
