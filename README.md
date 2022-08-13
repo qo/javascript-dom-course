ac485b4d8035c06682eb67d5f4c7980c1d636975
qo <72982727+qo@users.noreply.github.com>
2022-08-13 20:39:27 +0300

    - Update README.md - Add Lesson 2 Project

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Lesson 1: Selecting Elements in the DOM
 
+Lesson 2: Styling an Element
+
+
---
+++ lesson-2/app.js
@@ -0,0 +1,10 @@
+// change color of the title
+
+const title = document.querySelector("#fav-movies-title");
+title.style.color = "#EB5C68"
+
+// change font size of list items
+
+const listItems = document.querySelectorAll(".fav-movies-list-item");
+listItems.forEach(item => item.style.fontSize = "17px");
+
